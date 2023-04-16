40c6370c3e8fda61a293694e24926211bd3c9826
Tia <90842451+Jusferg@users.noreply.github.com>
2023-04-16 16:52:00 -0400

    created fahrenheit and celsius functions to display temperature

--- src/index.js
+++ src/index.js
@@ -34,6 +34,26 @@ function search(city) {
     let cityInput = document.querySelector("#city-input");
     search(cityInput.value);
   }
+
+  function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+  
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+
+  function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+  
+  let celsiusTemperature = null;
   
   function getTemperature(response) {
     console.log(response.data);
@@ -43,6 +63,8 @@ function search(city) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let iconElement = document.querySelector("#icon");
+
+    celsiusTemperature = response.data.main.temp;
   
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -52,12 +74,17 @@ function search(city) {
   
     iconElement.setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
   }
 
   let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
+  form.addEventListener("submit", handleSubmit);
 
-search("New York");
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+  let celsiusLink = document.querySelector("celsius-link");
+  celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+  search("New York");
